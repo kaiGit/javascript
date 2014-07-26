09415ac0a11415c92092f59830182ac6db2f7b19
kaiGit <kezhongseu@gmail.com>
2014-07-27 02:34:36 +0800

    remove test script written to do performance test.
    It turns out that the large data set and Floyd-warShall used too much memory, that naive implementation cannot allocate enough memory.

--- test2.js
+++
@@ -1,4 +0,0 @@
-var a41 = require('./assignment_4_1.js');
-
-var graph = a41.readGraphFromFile('./testdata/test4_1_3.txt');
-var a41Result = a41.computeAllPairShortestPath_FloydWarshall(graph);
--- test4.js
+++
@@ -1,4 +0,0 @@
-var a41 = require('./assignment_4_1.js');
-
-var graph = a41.readGraphFromFile('./testdata/test4_1_large.txt');
-var a41Result = a41.computeAllPairShortestPath_FloydWarshall(graph);
