393ed75941f8699f8831979ff7a0c595f4627b4b
kaiGit <kezhongseu@gmail.com>
2014-08-03 05:13:21 +0800

    assignment 5. Cannot run on large data set due to node.js's memory limitation.

---
+++ test_asm_5_1.js
@@ -0,0 +1,22 @@
+var a5 = require('./assignment_5_1.js');
+
+//console.log('select([1,2,3,4,5], 1)', a5.select([1,2,3,4,5], 1));
+
+//a5.assignment41('./testdata/tsp.small4_16898.1.txt');
+
+
+//a5.assignment41('./testdata/tsp.small2.txt');
+a5.assignment41('./testdata/tsp.txt');
+
+//a5.select([1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 25], 13);
+
+/*
+console.log(a5.key([1, 2], 1));
+console.log(a5.key([1, 2], 2));
+console.log(a5.key([1], 1));
+
+console.log(a5.key([1, 2], 0));
+console.log(a5.key([2], 0));
+console.log(a5.key([1], 0));
+console.log(a5.key([], 0));
+*/
